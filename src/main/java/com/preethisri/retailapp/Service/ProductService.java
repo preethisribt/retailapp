3b387e0889612b3130ae5d128e259edea55d0e56
preethisribt <preethisribt@gmail.com>
2026-07-14 14:14:03 +1000

    refactor: improve ProductService

--- src/main/java/com/preethisri/retailapp/Service/ProductService.java
+++ src/main/java/com/preethisri/retailapp/Service/ProductService.java
@@ -9,35 +9,44 @@ import com.preethisri.retailapp.Entity.Product;
 import com.preethisri.retailapp.Exception.ResourceNotFoundException;
 import com.preethisri.retailapp.Repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
 
-
+    @Transactional(readOnly = true)
     public List<ProductDTOResponse> getAllProduct() {
         List<Product> products = productRepository.findAll();
         return products.stream().map(productMapper::toDTO).toList();
     }
 
-    public ProductDTOResponse getProductByID(Long id) {
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Product not found for the id " + id));
+    private Product findProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.warn("Product with id {} not found", id);
+                    return new ResourceNotFoundException("Product not found for the id " + id);
+                });
+    }
 
+    @Transactional(readOnly = true)
+    public ProductDTOResponse getProductById(Long id) {
+        Product product = findProductById(id);
         return productMapper.toDTO(product);
     }
 
-    public List<ProductDTOResponse> getProductByName(List<String> names) {
-        List<Product> result = new ArrayList<>();
+    @Transactional(readOnly = true)
+    public List<ProductDTOResponse> getProducts(List<String> names) {
+        Set<Product> result = new LinkedHashSet<>();
 
         for (String name : names) {
             result.addAll(productRepository.findByProductNameContainingIgnoreCase(name));
@@ -45,24 +54,36 @@ public class ProductService {
         return result.stream().map(productMapper::toDTO).toList();
     }
 
+    @Transactional(readOnly = true)
     public List<ProductDTOResponse> getByCategory(String name) {
         List<Product> products = productRepository.findByCategoryContainingIgnoreCase(name);
         return products.stream().map(productMapper::toDTO).toList();
     }
 
-    public ProductDTOResponse addNewProduct(ProductDTORequest data) {
-        Product entity = productMapper.toEntity(data);
+    @Transactional
+    public ProductDTOResponse addNewProduct(ProductDTORequest request) {
+        Product entity = productMapper.toEntity(request);
 
-        if (checkDuplicateProduct(entity))
+        if (checkDuplicateProduct(entity)) {
+            log.warn("Duplicate product found {}", entity.getProductName());
             throw new ProductAlreadyExistsException("Product already exists");
-        else {
-            entity.setSku(generateSKU(entity));
+        }
+
+        log.info("Creating Product {}", entity.getProductName());
+        entity.setSku(generateSKU(entity));
+
+        try {
             Product product = productRepository.save(entity);
+            log.debug("Product created successfully with id {}", product.getId());
+
             return productMapper.toDTO(product);
+        } catch (DataIntegrityViolationException exception) {
+            log.error("Failed to create product {} due to database constraint violation", entity.getProductName());
+            throw new ProductAlreadyExistsException("Product already exists");
         }
     }
 
-    public boolean checkDuplicateProduct(Product product) {
+    private boolean checkDuplicateProduct(Product product) {
         return productRepository.existsByProductNameAndCategoryAndColourAndStorageAndPrice(product.getProductName(),
                 product.getCategory(),
                 product.getColour(),
@@ -70,7 +91,7 @@ public class ProductService {
                 product.getPrice());
     }
 
-    public String generateSKU(Product product) {
+    private String generateSKU(Product product) {
         String productName = product.getProductName().substring(0, Math.min(3, product.getProductName().length())).toUpperCase();
         String category = product.getCategory().substring(0, Math.min(3, product.getCategory().length())).toUpperCase();
         String suffix = UUID.randomUUID().toString()
@@ -80,71 +101,132 @@ public class ProductService {
         return productName + "-" + category + "-" + suffix;
     }
 
-    public ProductDTOResponse updateProduct(long id, ProductDTORequest data) {
-        Product existingProduct = productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found for the id " + id));
+    @Transactional
+    public ProductDTOResponse updateProduct(long id, ProductDTORequest request) {
+        Product existingProduct = findProductById(id);
+        log.info("Updating product with id {}", id);
 
-        existingProduct.setProductName(data.getProductName());
-        existingProduct.setPrice(data.getPrice());
-        existingProduct.setDescription(data.getDescription());
-        existingProduct.setStock(data.getStock());
-        existingProduct.setCategory(data.getCategory());
-        existingProduct.setColour(data.getColour());
-        existingProduct.setStorage(data.getStorage());
+        existingProduct.setProductName(request.getProductName());
+        existingProduct.setPrice(request.getPrice());
+        existingProduct.setDescription(request.getDescription());
+        existingProduct.setStock(request.getStock());
+        existingProduct.setCategory(request.getCategory());
+        existingProduct.setColour(request.getColour());
+        existingProduct.setStorage(request.getStorage());
 
         Product updatedProduct = productRepository.save(existingProduct);
+        log.info("Product {} updated successfully", id);
+
         return productMapper.toDTO(updatedProduct);
     }
 
-    public ProductDTOResponse partialUpdateProduct(long id, ProductDTOPatchRequest data) {
-        Product existingProduct = productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found for the id " + id));
+    @Transactional
+    public ProductDTOResponse partialUpdateProduct(long id, ProductDTOPatchRequest request) {
+        Product existingProduct = findProductById(id);
+        log.info("Updating product with id {}", id);
 
-        if (data.getProductName() != null) {
-            if (data.getProductName().isBlank())
+        updateProductName(existingProduct, id, request);
+        updatePrice(existingProduct, id, request);
+        updateStock(existingProduct, id, request);
+        updateCategory(existingProduct, id, request);
+        updateColour(existingProduct, id, request);
+        updateStorage(existingProduct, id, request);
+        updateDescription(existingProduct, id, request);
+
+
+        Product updatedProduct = productRepository.save(existingProduct);
+        log.info("Product {} updated successfully", id);
+
+        return productMapper.toDTO(updatedProduct);
+    }
+
+    private void updateProductName(Product existingProduct, long id, ProductDTOPatchRequest request) {
+        if (request.getProductName() != null) {
+            if (request.getProductName().isBlank()) {
+                log.warn("Product name is blank for {}", id);
                 throw new IllegalArgumentException("Product name can't be blank");
-            existingProduct.setProductName(data.getProductName());
-        }
+            }
 
-        if (data.getPrice() != null) {
-            if (data.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+            existingProduct.setProductName(request.getProductName());
+            log.debug("ProductName {} updated successfully for {}", request.getProductName(), id);
+        }
+    }
+
+    private void updatePrice(Product existingProduct, long id, ProductDTOPatchRequest request) {
+        if (request.getPrice() != null) {
+            if (request.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+                log.warn("Price is 0 or negative for {}", id);
                 throw new IllegalArgumentException("Price must be greater than 0");
             }
 
-            existingProduct.setPrice(data.getPrice());
+            existingProduct.setPrice(request.getPrice());
+            log.debug("Price {} updated successfully for {}", request.getPrice(), id);
         }
+    }
 
-        if (data.getDescription() != null) {
-            if (data.getDescription().isBlank())
-                throw new IllegalArgumentException("description can't be blank");
-            existingProduct.setDescription(data.getDescription());
-        }
 
-        if (data.getStock() != null) {
-            if (data.getStock() < 0)
+    private void updateStock(Product existingProduct, long id, ProductDTOPatchRequest request) {
+        if (request.getStock() != null) {
+            if (request.getStock() < 0) {
+                log.warn("Stock is negative for {}", id);
                 throw new IllegalArgumentException("Stock can't be negative");
-            existingProduct.setStock(data.getStock());
-        }
+            }
 
-        if (data.getCategory() != null) {
-            if (data.getCategory().isBlank())
+            existingProduct.setStock(request.getStock());
+            log.debug("Stock {} updated successfully for product {}", request.getStock(), id);
+        }
+    }
+
+    private void updateCategory(Product existingProduct, long id, ProductDTOPatchRequest request) {
+        if (request.getCategory() != null) {
+            if (request.getCategory().isBlank()) {
+                log.warn("Category is blank for {}", id);
                 throw new IllegalArgumentException("Category can't be blank");
-            existingProduct.setCategory(data.getCategory());
-
+            }
+            existingProduct.setCategory(request.getCategory());
+            log.debug("Category {} updated successfully for product {}", request.getCategory(), id);
         }
+    }
 
-        if (data.getColour() != null) {
-            if (data.getColour().isBlank())
+    private void updateColour(Product existingProduct, long id, ProductDTOPatchRequest request) {
+        if (request.getColour() != null) {
+            if (request.getColour().isBlank()) {
+                log.warn("Colour is blank for {}", id);
                 throw new IllegalArgumentException("Colour can't be blank");
-            existingProduct.setColour(data.getColour());
-        }
+            }
 
-        if (data.getStorage() != null) {
-            if (data.getStorage().isBlank())
+            existingProduct.setColour(request.getColour());
+            log.debug("Colour {} updated successfully for {}", request.getColour(), id);
+        }
+    }
+
+    private void updateDescription(Product existingProduct, long id, ProductDTOPatchRequest request) {
+        if (request.getDescription() != null) {
+            if (request.getDescription().isBlank()) {
+                log.warn("Description is blank for product {}", id);
+                throw new IllegalArgumentException("description can't be blank");
+            }
+            existingProduct.setDescription(request.getDescription());
+            log.debug("Description {} updated successfully for {}", request.getDescription(), id);
+        }
+    }
+
+    private void updateStorage(Product existingProduct, long id, ProductDTOPatchRequest request) {
+        if (request.getStorage() != null) {
+            if (request.getStorage().isBlank()) {
+                log.warn("Storage is blank for {}", id);
                 throw new IllegalArgumentException("Storage can't be blank");
+            }
 
-            existingProduct.setStorage(data.getStorage());
+            existingProduct.setStorage(request.getStorage());
+            log.debug("Storage {} updated successfully for {}", request.getStorage(), id);
         }
+    }
 
-        Product updatedProduct = productRepository.save(existingProduct);
-        return productMapper.toDTO(updatedProduct);
+    @Transactional
+    public void deleteProduct(long id) {
+        findProductById(id);
+        productRepository.deleteById(id);
+        log.info("Product {} deleted successfully", id);
     }
 }
